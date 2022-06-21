3899f264bffc909aa5d4e93b9eb29922b36ef302
SlipHD <zuraffa2015@gmail.com>
2022-06-20 21:17:00 -0300

    Ready for production

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -24,7 +24,7 @@ public class RecordService {
 	@Autowired
 	private GameRepository gameRepository;
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public RecordDTO insert(RecordInsertDTO dto) {
 		Record entity = new Record();
 		Game game = gameRepository.getReferenceById(dto.getGameId());
